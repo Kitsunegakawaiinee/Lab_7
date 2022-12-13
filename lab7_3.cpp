97413656bde165a7ad50709dafef041f95af6eee
SKOT <skotskip@gmail.com>
2022-12-13 17:18:59 +0700

    ทำข้อ ข้อ 4 เสร็จ (4 จริงๆที่ไม่ใช่ 5)

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,15 +1,8 @@
 #include<iostream>
+#include<cmath>
 
 using namespace std;
 
+int adiff(int x, int y){  int ans = 0;  ans = (x >= y)? x-y:y-x;  ans %= 360;  if (ans > 180) ans = 360-ans;  return ans;}
 
-int main(){
-  cout << adiff(180,270);
-  cout << adiff(210,45);
-  cout << adiff(0,360);
-  cout << adiff(10,350);
-  cout << adiff(95,260);
-  cout << adiff(90,-90);
-  cout << adiff(1000,280);
-  cout << adiff(60,244);
-}
+int main(){  cout << adiff(180,270) << endl;  cout << adiff(210,45)<< endl;  cout << adiff(0,360) << endl;  cout << adiff(10,350) << endl;  cout << adiff(95,260) << endl;  cout << adiff(90,-90) << endl;  cout << adiff(1000,280) << endl;  cout << adiff(60,244) << endl;}
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,48 +1,9 @@
-#include<iostream>
-#include<string>
 
-using namespace std;
-
-string func1(string x){
-	int L = x.size();
-	string y = "";
-	for (int i ; i < L ; i++)
-	{
-		y += x[L-i-1];
-	}
-	return y;
-}
-
-string func2(string x){
-	int i = 0, L = x.size();
-	string y = "";
-	while(i < L){
-		y += toupper(x[i]);
-		i++;
-	}
-	return y;	
-}
-
-string func3(string x){
-	int i = 0, L = x.size();
-	string y = "";
-	while(i < L){
-		y += tolower(x[i]);
-		i++;
-	}
-	return y;	
-}
-
-int main()
-{
-	string A,B,c,a;
-    cout << "Input text: " ;
-	cin >> A;
-	B = func3(A);
-	c = func3(func1(A));
-	a = (c == B)? a="Yes": a = "No";
-
-    cout << "Reversed text: " << func1(A) << endl;
-    cout << "Palindrome: " << a ;
-    return 0;
-}
+  cout << adiff(180,270);
+  cout << adiff(210,45);
+  cout << adiff(0,360);
+  cout << adiff(10,350);
+  cout << adiff(95,260);
+  cout << adiff(90,-90);
+  cout << adiff(1000,280);
+  cout << adiff(60,244);
