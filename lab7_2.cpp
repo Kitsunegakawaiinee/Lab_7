11fd8c3e8c46dc5f9ce34139203f3e096d834678
SKOT <skotskip@gmail.com>
2022-12-13 16:02:26 +0700

    เสร็จ ข้อ 2

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -4,11 +4,39 @@ using namespace std;
 
 int main(){
 	
-	int i = 0, P[5000], k, result;
-	while(i < 5000){
+	int P[5000], k, result;
+
+	for (int i = 0; i < 5000; i++)
+	{
 		P[i] = 2*i+1;
-		i++;
 	}
+
+	cout << "Please input k: ";
+	cin >> k;
 	
+
+	if (k > 0)
+	{
+		int i =0;
+		while (i<5000)
+		{
+			if (P[i]%k == 0)
+			{
+				result -= P[i];
+			}
+			else 
+			{
+				result += P[i];
+			}
+
+			i++;
+		}
+
+		cout << "Result = "<< result;
+	}
+	else
+	{
+		cout << "Invalid input!!!";
+	}
 	return 0;
 }
